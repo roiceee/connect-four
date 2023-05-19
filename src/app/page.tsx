b515ad3a89fe5cedc42108b6555c3696b3ab345d
jroiceee <jroicealdeza@gmail.com>
2023-05-19 18:32:01 +0800

    finish instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,18 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import Container from "react-bootstrap/Container";
 import PlayButton from "@/components/play-button/play-button";
 import Board from "@/components/board/board";
+import Instruction from "@/components/instruction/instruction";
 
 export default function Home() {
-
   const [audio, setAudio] = useState<null | HTMLAudioElement>(null);
+
+  const [isInstructionState, setIsInstructionState] = useState<boolean>(false);
 
   const [isPlayingState, setIsPlayingState] = useState<boolean>(false);
 
   const setIsPlaying = useCallback(() => {
     setIsPlayingState(true);
+    setIsInstructionState(false);
     if (!audio) {
       return;
     }
@@ -24,41 +27,51 @@ export default function Home() {
 
   useEffect(() => {
     setAudio(new Audio("/background.MP3"));
-    window.onbeforeunload = function() {
-      return ""
-    }
-  },[])
+    window.onbeforeunload = function () {
+      return "";
+    };
+  }, []);
+
+  const setToInstruction = useCallback(() => {
+    setIsInstructionState(true);
+  }, []);
+
   return (
-  
     <Container>
       <div style={{ textAlign: "center" }}>
         <Image
           src={"/images/connect-4.png"}
           height={200}
-          width={300}        
+          width={300}
           style={{ objectFit: "contain" }}
           alt="title"
           priority={true}
         />
       </div>
 
-      {!isPlayingState && (
+      {(!isInstructionState && !isPlayingState) && (
         <div
           className="text-center"
           style={{ position: "absolute", top: "80%", left: "0", right: "0" }}
         >
-          <PlayButton onClick={setIsPlaying} />
+          <PlayButton onClick={setToInstruction} />
         </div>
       )}
+
       {isPlayingState && (
         <Container>
           <div className="d-flex justify-content-center">
             <Board audio={audio} />
+           
           </div>
         </Container>
       )}
-      
+
+      {isInstructionState && (
+        <div className="d-flex justify-content-center">
+          <Instruction onClick={setIsPlaying} />
+        </div>
+      )}
     </Container>
-    
   );
 }
---
+++ src/components/instruction/instruction.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import { Button} from "react-bootstrap";
+
+interface InstructionProps {
+    onClick: () => void;
+}
+
+
+function Instruction({onClick} : InstructionProps) {
+  return (
+    <div className="bg-black p-3 rounded-3 bg-opacity-75 d-flex flex-column">
+      <div className="d-flex flex-column align-items-center">
+        <h2 className="text-center">How to play?</h2>
+        <ul>
+          <li>Play with a friend.</li>
+          <li>Win the game by placing four same-colored blocks in a row.</li>
+        </ul>
+      </div>
+      <div className="gap-3 text-center justify-content-center d-flex flex-column flex-md-row">
+        <div>
+          <Image
+            src={"/images/ins1.png"}
+            alt="Horizontal"
+            height={120}
+            width={120}
+          />
+          <div>Horizontal</div>
+        </div>
+        <div>
+          <Image
+            src={"/images/ins3.png"}
+            alt="Vertical"
+            height={120}
+            width={120}
+          />
+          <div>Vertical</div>
+        </div>
+        <div>
+          <Image
+            src={"/images/ins2.png"}
+            alt="Diagonal"
+            height={120}
+            width={120}
+          />
+          <div className="text-center">Diagonal</div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center mt-3">
+        <Button className="w-75" variant="light" onClick={onClick}>Continue</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Instruction;
